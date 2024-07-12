0524efa3de215a4871e181ab6d495df904bc5b50
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-12 20:33:21 +0530

    this is the index.js file changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port | 8000;
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.send("hello world this is my get route brother");
 });
 
 app.listen(port, (err) => {
