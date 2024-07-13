28cd078e617d13f1c5e785b65faffad4154a94a7
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-13 21:45:29 +0530

    this is the commit in the index.js file

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors")
-const port = process.env.port || 8000;
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8000;
 
 require('dotenv').config();
 const registerRoute = require("./routes/SignupRoutes/SignupRoutes");
-const loginRoutes = require("./routes/LoginRoutes/LoginRoutes")
-
-
+const loginRoutes = require("./routes/LoginRoutes/LoginRoutes");
 
 app.use(cors());
 app.use(express.json());
-// Use express.urlencoded middleware
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/", registerRoute);
 app.use("/", loginRoutes);
@@ -23,18 +19,24 @@ app.get("/", (req, res) => {
     res.send("hello world this is my get route brother");
 });
 
+// Database connection
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://<username>:<password>@<cluster-address>/<dbname>?retryWrites=true&w=majority";
 
-
-// // database connections 
-mongoose.connect("mongodb+srv://raykushwaha0031:C1k4maJXzH2vAmh4@blog.zlf5agh.mongodb.net/ProjectX").then(() => {
-    console.log("Database connected succesfully")
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Increase this value if necessary
+    socketTimeoutMS: 45000 // Increase this value if necessary
+}).then(() => {
+    console.log("Database connected successfully");
 }).catch((err) => {
-    console.log(err)
+    console.error("Database connection error:", err);
 });
 
-
-
-
 app.listen(port, (err) => {
-    console.log("server is listening at port 8000");
+    if (err) {
+        console.error("Server error:", err);
+    } else {
+        console.log(`Server is listening at http://localhost:${port}`);
+    }
 });
